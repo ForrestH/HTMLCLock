9097bdead47c600e11f6e6d81f63c3e63efc31f1
Forrest Hansen <flibertygibit@gmail.com>
2015-01-16 19:42:01 -0800

    first commit

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+function getTime() {
+	console.log("updating time");
+	var date = new Date();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	var period = "am";
+	if(hours > 12) {
+		hours = hours - 12;
+		period = "pm";
+	}
+	var clockElement = document.getElementById("clock");
+	clockElement.innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds) + "&nbsp" + period;
+	setTimeout(getTime, 1000);
+}
+
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
