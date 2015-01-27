1434fcf20025789cb43f38d1991a907bd1fe3718
ForrestH <flibertygibit@gmail.com>
2015-01-27 14:36:20 -0800

    Added alarm text

--- js/clock.js
+++ js/clock.js
@@ -4,14 +4,23 @@ function getTime() {
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
 	var seconds = date.getSeconds();
+	var deciseconds = Math.round(date.getMilliseconds() / 100);
+	if(deciseconds == 10)
+	{
+		deciseconds = 0;
+	}
 	var period = "am";
+	
 	if(hours > 12) {
 		hours = hours - 12;
 		period = "pm";
 	}
 	var clockElement = document.getElementById("clock");
-	clockElement.innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds) + "&nbsp" + period;
-	setTimeout(getTime, 1000);
+	
+	clockElement.innerHTML = pad(hours) + ":" + pad(minutes) + 
+		":" + pad(seconds) + "." + deciseconds + "&nbsp" + period;
+		
+	setTimeout(getTime, 1);
 }
 
 function pad(n) {
