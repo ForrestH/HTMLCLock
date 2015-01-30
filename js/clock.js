23c5b42f6d055903c21eee82ac435dd81d131543
ForrestH <flibertygibit@gmail.com>
2015-01-29 19:34:12 -0800

    Added weather feature for Building 14 at Cal Poly SLO

--- js/clock.js
+++ js/clock.js
@@ -13,10 +13,9 @@ function getTime() {
 	if(hours < 1) {
 		hours = hours + 12;
 	}
-	var clockElement = document.getElementById("clock");
-	
-	clockElement.innerHTML = pad(hours) + ":" + pad(minutes) + 
-		":" + pad(seconds) + "&nbsp" + period;
+
+	$("#clock").html(pad(hours) + ":" + pad(minutes) + 
+		":" + pad(seconds) + "&nbsp" + period);
 		
 	setTimeout(getTime, 1000);
 }
@@ -24,3 +23,33 @@ function getTime() {
 function pad(n) {
     return (n < 10) ? ("0" + n) : n;
 }
+
+
+
+$(document).ready(function() {
+			$.getJSON (
+				"https://api.forecast.io/forecast/6206d2099d97ff183ccbdb62f468520e/35.300399,-120.662362",
+				"callback=?",
+				function(data, textStatus, jqXHR) {
+					$("#forecastLabel").html(data.daily.data[0].summary);
+					$("#forecastIcon").attr("src", "img/" + data.daily.data[0].icon + ".png");
+					var tempMax = data.daily.data[0].temperatureMax;
+					if(tempMax >= 90) {
+						$("body").addClass("hot");
+					}
+					else if(tempMax >= 80) {
+						$("body").addClass("warm");
+					}
+					else if(tempMax >= 70) {
+						$("body").addClass("nice");
+					}
+					else if(tempMax >= 60) {
+						$("body").addClass("chilly");
+					}
+					else {
+						$("body").addClass("cold");
+					}
+					$("#forecastLabel").append("<br>High of " + tempMax + " &degF");
+				}
+			);
+});
