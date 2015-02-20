14da31345033ba96db2c208b23b10ab6d93d0920
ForrestH <flibertygibit@gmail.com>
2015-02-19 16:18:46 -0800

    trying making weather init it's own function that gets called on body load

--- js/clock.js
+++ js/clock.js
@@ -171,33 +171,33 @@ function pad(n) {
     return (n < 10) ? ("0" + n) : n;
 }
 
-$(document).ready(function() {
-alert("penis");
-			$.getJSON (
-				"https://api.forecast.io/forecast/6206d2099d97ff183ccbdb62f468520e/35.300399,-120.662362",
-				"callback=?",
-				function(data, textStatus, jqXHR) {
-					$("#forecastLabel").html(data.daily.data[0].summary);
-					$("#forecastIcon").attr("src", "img/" + data.daily.data[0].icon + ".png");
-					var tempMax = data.daily.data[0].temperatureMax;
-					if(tempMax >= 90) {
-						$("body").addClass("hot");
-					}
-					else if(tempMax >= 80) {
-						$("body").addClass("warm");
-					}
-					else if(tempMax >= 70) {
-						$("body").addClass("nice");
-					}
-					else if(tempMax >= 60) {
-						$("body").addClass("chilly");
-					}
-					else {
-						$("body").addClass("cold");
-					}
-					$("#forecastLabel").append("<br>High of " + tempMax + " &degF");
-				}
-			);
-			
-			getAllAlarms();
-});
+function weatherInit() {
+	alert("penis");
+	$.getJSON (
+		"https://api.forecast.io/forecast/6206d2099d97ff183ccbdb62f468520e/35.300399,-120.662362",
+		"callback=?",
+		function(data, textStatus, jqXHR) {
+			$("#forecastLabel").html(data.daily.data[0].summary);
+			$("#forecastIcon").attr("src", "img/" + data.daily.data[0].icon + ".png");
+			var tempMax = data.daily.data[0].temperatureMax;
+			if(tempMax >= 90) {
+				$("body").addClass("hot");
+			}
+			else if(tempMax >= 80) {
+				$("body").addClass("warm");
+			}
+			else if(tempMax >= 70) {
+				$("body").addClass("nice");
+			}
+			else if(tempMax >= 60) {
+				$("body").addClass("chilly");
+			}
+			else {
+				$("body").addClass("cold");
+			}
+			$("#forecastLabel").append("<br>High of " + tempMax + " &degF");
+		}
+	);
+	
+	getAllAlarms();
+}
